ac781d2abb442457d13aa6843067b94009bcde15
PhanTanCuong <phantancuong2002@gmail.com>
2025-10-02 08:31:27 +0700

    create product controller
    prompt " php artisan make:controller ProductsController --resource

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+
+        return view('products.index',['products'=>$products]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $product = new Product();
+
+        $product->product_id = $request->input('product_id');
+        $product->product_name = $request->input('product_name');
+        $product->price = $request->input('price');
+        // $product->add();
+        // return redirect('/products');
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
