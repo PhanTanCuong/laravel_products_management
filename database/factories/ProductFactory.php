50365ae89dd8a0e19d68ac3d4c691dace1e1a48e
PhanTanCuong <phantancuong2002@gmail.com>
2025-10-02 08:31:49 +0700

    Create ProductsFactory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    protected $model = \App\Models\Product::class;
+
+    public function definition(): array
+    {
+        return [
+        'product_id' => $this->faker->unique()->randomNumber(),
+        'product_name' => $this->faker->words(3, true),
+        'price' => $this->faker->randomFloat(2,  100, 1000   ),
+        'created_at'=>now(),
+        ];
+    }
+}
